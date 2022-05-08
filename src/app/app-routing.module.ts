5ee6cc7c7080fc7ff59ed76ed19b90dc278ef512
calvinsim2 <88623537+calvinsim2@users.noreply.github.com>
2022-05-08 23:32:17 +0800

    added base components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((a) => a.LoginModule),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./signup/signup.module').then((a) => a.SignupModule),
+  },
+  {
+    path: 'management',
+    loadChildren: () =>
+      import('./management/management.module').then((a) => a.ManagementModule),
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
