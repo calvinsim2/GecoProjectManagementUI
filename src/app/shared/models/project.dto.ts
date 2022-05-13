1a64c6124b4ec0074da95320e84acfda5e8b2728
Beaumont Phua <mpsslh@hotmail.com>
2022-05-13 14:43:16 +0800

    Modify client to show client name instead of client ID

--- src/app/shared/models/project.dto.ts
+++ src/app/shared/models/project.dto.ts
@@ -1,3 +1,8 @@
+type Client = {
+  clientID: number;
+  clientName: string;
+};
+
 export class ProjectDto {
   projectID!: number;
   projectName!: string;
@@ -8,4 +13,5 @@ export class ProjectDto {
   projectDescription!: string;
   clientID!: number;
   ProjectManagerID!: number;
+  client!: Client;
 }
